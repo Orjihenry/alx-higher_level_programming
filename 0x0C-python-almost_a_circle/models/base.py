c50e39e660d5e4938889a6043a3d4207aa3588dd
Henry <orjihen@yahoo.com>
2023-04-20 22:37:52 +0000

    bug fix

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My first class """
+
+
+class Base:
+    __nb_objects = 0  # private class attribute
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
