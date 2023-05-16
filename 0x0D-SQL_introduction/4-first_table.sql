3b5b6efbf7d8abaef1aa49883d062ffead962a19
Orjihenry <orjihen@yahoo.com>
2023-05-17 00:20:26 +0100

    First table 🙃😉😌😜

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Script that creates a table called first_table in mysql server
+-- 'IF NOT EXISTS' allows program to run without errors if db exist
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
