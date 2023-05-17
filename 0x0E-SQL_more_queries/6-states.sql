14cac807011f976843a99c1e4e5ae648a68b38ff
Orjihenry <orjihen@yahoo.com>
2023-05-17 22:43:58 +0100

    States table 🌍🌎🌏

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- Script that creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- switch to database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- creates the table states
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
+);
