ad0b1ccdbf1c8c412d8a61d40b47ef6f221bf07b
Orjihenry <orjihen@yahoo.com>
2023-06-14 21:34:06 +0100

    bug fix - 3 languages 😁👌

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const c = 'C is fun'
-const py = 'Python is cool'
-const js = 'JavaScript is amazing'
+const c = 'C is fun';
+const py = 'Python is cool';
+const js = 'JavaScript is amazing';
 
-console.log(c + '\n' + py + '\n' + js)
+console.log(c + '\n' + py + '\n' + js);
