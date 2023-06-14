463a0caddf62e06e399e8bfdf5ad0f473e8b52d5
Orjihenry <orjihen@yahoo.com>
2023-06-14 01:55:48 +0100

    Argumentse ✅✅✅

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const args = process.argv.length;
 
 if (args === 2) {
-	console.log('No argument);
+	console.log('No argument');
 } else if (args === 3) {
 	console.log('Argument found');
 } else {
