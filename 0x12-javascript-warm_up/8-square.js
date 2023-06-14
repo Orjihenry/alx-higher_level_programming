d629459c6c75310a49c9c71f98a4022aaad93153
Orjihenry <orjihen@yahoo.com>
2023-06-15 00:57:06 +0100

    Square ✌️ 😎☺️

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const size = parseInt(args);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      console.log("X".repeat(size));
+    }
+  }
+}
