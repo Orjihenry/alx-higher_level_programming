d80118cfedd3da627dc978757ce0e241555ec329
Orjihenry <orjihen@yahoo.com>
2023-06-16 02:30:29 +0100

    Rectangle #2 👻👻👻

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 && h <= 0) {
-      return {};
-    } else {
-      this.width = w;
-      this.height = h;
+    if (w > 0 && h > 0) {
+      [this.width = w,
+        this.height = h] =
+      [w, h];
     }
   }
 };
