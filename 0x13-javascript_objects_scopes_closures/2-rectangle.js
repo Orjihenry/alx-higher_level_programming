2060ad3e9882210a5f096c7541767609ba224e1d
Orjihenry <orjihen@yahoo.com>
2023-06-16 02:22:47 +0100

    Rectangle #2 👻👻👻

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || Number.isNaN(w) || Number.isNaN(h)) {
+    if (w <= 0 && h <= 0 && Number.isNaN(w) && Number.isNaN(h)) {
       return {};
     } else {
       this.width = w;
