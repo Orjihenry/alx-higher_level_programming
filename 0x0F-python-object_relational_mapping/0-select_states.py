8d733faf6e45f255c8d4b3eae6ab43c7d2273900
Orjihenry <orjihen@yahoo.com>
2023-06-18 23:47:43 +0100

    Get all states 💯💫⭐

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that lists all states in db = hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':   # Run code only when executed directly.
+    """
+    Get the states from the database
+    when connection has been established.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()   # Cursor object to execute queries.
+
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
