b2df3ba8e6615b958736dc8ee265816f6999f4fb
Orjihenry <orjihen@yahoo.com>
2023-06-19 00:00:56 +0100

    Filter states 😎✌️ 👌

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all states in db = hbtn_0e_0_usa.
+Only lists states starting with upper N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':   # Run code only when executed directly.
+    """
+    Get the states from the database
+    when connection has been established.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])   # Establishes connection
+
+    db_cursor = db_connect.cursor()   # Cursor object to execute queries.
+
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE N%")   # Filters only states starting with upper N
+
+    states = db_cursor.fetchall()  # Checks whole data and displays only result
+
+    for state in states:
+        print(state)
