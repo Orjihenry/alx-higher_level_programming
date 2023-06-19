8ca6b18053f4f544263a3a9246c18c17cae8e20a
Orjihenry <orjihen@yahoo.com>
2023-06-19 14:09:27 +0100

    Hot fix - Filter states 😎✌️ 👌

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,20 +7,19 @@ Only lists states starting with upper N
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':   # Run code only when executed directly.
+if __name__ == '__main__':
     """
-    Get the states from the database
-    when connection has been established.
+    Access to the database and get the states
+    from the database.
     """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC")
-
-    states = db_cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
+    state = cur.fetchall()
 
     for state in states:
         print(state)
